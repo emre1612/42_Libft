62648286a12dad6e7248fde45de54beb456e794a
emre1612 <immortalsensei832@gmail.com>
2025-06-19 22:39:58 +0300

    güncel

--- ft_split.c
+++ ft_split.c
@@ -6,78 +6,106 @@
 /*   By: emkaymak <emkaymak@student.42.kocaeli.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 21:51:34 by emkaymak          #+#    #+#             */
-/*   Updated: 2025/05/27 22:15:28 by emkaymak         ###   ########.fr       */
+/*   Updated: 2025/06/19 22:24:40 by emkaymak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(const char *s, char c)
+static int	ft_strcount(char const *s, char c)
 {
-	int count = 0;
-	int i = 0;
+	int	i;
+	int	count;
 
+	i = 0;
+	while (s[i] && s[i] == c)
+		i++;
+	if (s[i] == '\0')
+		return (0);
+	count = 1;
 	while (s[i])
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		if (s[i] && s[i] != c)
-		{
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
 			count++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
+		i++;
 	}
 	return (count);
 }
 
-static char	*word_dup(const char *s, int start, int end)
+static int	ft_charcount(char const *s, char c)
 {
-	char	*word;
-	int		i = 0;
+	int	i;
 
-	word = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (!word)
-		return (NULL);
-	while (start < end)
-		word[i++] = s[start++];
-	word[i] = '\0';
-	return (word);
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
 }
 
-static void	free_all(char **result, int i)
+static char	**ft_free_malloc(char **tab)
 {
-	while (i >= 0)
-		free(result[i--]);
-	free(result);
+	size_t	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
-	int		i = 0, j = 0, start = -1;
+	char	**ptr;
+	int		x;
+	int		y;
 
-	if (!s)
+	ptr = (char **)malloc(sizeof(char *) * (ft_strcount(s, c) + 1));
+	if (!ptr)
 		return (NULL);
-	result = (char **)malloc(sizeof(char *) * (word_count(s, c) + 1));
-	if (!result)
-		return (NULL);
-	while (s[i])
+	x = -1;
+	while (*s)
 	{
-		if (s[i] != c && start == -1)
-			start = i;
-		else if ((s[i] == c || s[i + 1] == '\0') && start != -1)
+		while (*s && *s == c)
+			s++;
+		if (*s)
 		{
-			result[j] = word_dup(s, start, (s[i] == c) ? i : i + 1);
-			if (!result[j++])
-			{
-				free_all(result, j - 2);
-				return (NULL);
-			}
-			start = -1;
+			ptr[++x] = (char *)malloc(sizeof(char) * (ft_charcount(s, c) + 1));
+			if (!ptr[x])
+				return (ft_free_malloc(ptr));
+			y = 0;
+			while (*s && *s != c)
+				ptr[x][y++] = *s++;
+			ptr[x][y] = '\0';
 		}
+	}
+	ptr[++x] = NULL;
+	return (ptr);
+}
+#include <stdio.h>
+
+int	main(void)
+{
+	char	**result;
+	int		i;
+
+	result = ft_split("  merhaba 42  okul  ", ' ');
+	if (!result)
+		return (1);
+	i = 0;
+	while (result[i])
+	{
+		printf("%s\n",result[i]);
 		i++;
 	}
-	result[j] = NULL;
-	return (result);
+	i = 0;
+	while (result[i])
+	{
+		free(result[i]);
+		i++;
+	}
+	free(result);
+	return (0);
 }
